73e102d5735b7f5295b45e81ba641065e3e8af47
Yashas_J <yashasjraj@gmail.com>
2024-06-13 17:01:04 +0530

    updated app.py to test continue deployment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "Welcome Yashas How are you"
+   return "Welcome Yashas How are you and Welcome Boy"
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
